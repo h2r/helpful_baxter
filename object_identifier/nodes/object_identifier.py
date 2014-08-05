82442326be8f4e9468a1ffeab593bee8766e3d3f
stephen <brawner@gmail.com>
2014-08-05 17:05:23 -0400

    adding updates

--- object_identifier/nodes/object_identifier.py
+++ object_identifier/nodes/object_identifier.py
@@ -3,24 +3,25 @@
 import roslib
 roslib.load_manifest("object_identifier")
 import rospy
+import copy
 
 from object_recognition_msgs.msg import RecognizedObjectArray, RecognizedObject
 from ar_track_alvar.msg import AlvarMarkers, AlvarMarker
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 defined_objects = [
-	{"class": "gyrobowl", "name": "cocoa_bowl"},
-	{"class": "gyrobowl", "name": "flour_bowl"},
-	{"class": "gyrobowl", "name": "salt_bowl"},
-	{"class": "gyrobowl", "name": "baking_powder_bovl"},
-	{"class": "gyrobowl", "name": "baking_soda_bowl"},
-	{"class": "gyrobowl", "name": "eggs_bowl"},
-	{"class": "gyrobowl", "name": "sugar_bowl"},
-	{"class": "gyrobowl", "name": "vanilla_bowl"},
-	{"class": "gyrobowl", "name": "butter_bowl"},
-	{"class": "mixing_bowl", "name": "mixing_bowl"},
-	{"class": "spoon", "name": "wooden_spoon"},
-	{"class": "spoon", "name": "wooden_spoon"},
+	{"class": "gyrobowl", "name": "cocoa_bowl"},# 0
+	{"class": "gyrobowl", "name": "flour_bowl"},# 1
+	{"class": "gyrobowl", "name": "salt_bowl"},# 2
+	{"class": "gyrobowl", "name": "baking_powder_bovl"},# 3
+	{"class": "gyrobowl", "name": "baking_soda_bowl"},# 4
+	{"class": "gyrobowl", "name": "eggs_bowl"},# 5
+	{"class": "gyrobowl", "name": "sugar_bowl"},# 6
+	{"class": "gyrobowl", "name": "vanilla_bowl"},# 7
+	{"class": "gyrobowl", "name": "butter_bowl"},# 8
+	{"class": "mixing_bowl", "name": "mixing_bowl"}, # 9
+	{"class": "spoon", "name": "spoon"},# 10
+	{"class": "spoon", "name": "plastic_spoon"},# 11
 ]
 
 
@@ -30,7 +31,7 @@ class ObjectIdentifier:
 		#self.joberlin_objects = []
 		#rospy.Subscriber("/recognized_object_array", self.ork_callback)
 		#rospy.Subscriber("/joberlin_detection", self.joberlin_callback)
-		rospy.Subscriber("/ar_markers", self.ar_marker_callback)
+		rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.ar_marker_callback)
 		self.ar_object_publisher = rospy.Publisher("/ar_objects", RecognizedObjectArray)
 
 	def ork_callback(self, msg):
@@ -41,10 +42,13 @@ class ObjectIdentifier:
 
 	def ar_marker_callback(self, msg):
 		objects_msg = RecognizedObjectArray()
-		objects_msg.header = msg.header
+
+		objects_msg.header = copy.deepcopy(msg.header)
 		for marker in msg.markers:
 			object = RecognizedObject()
+			
 			object.pose = self.get_pose_from_pose(marker.pose)
+			object.pose.header = copy.deepcopy(marker.header)
 			object.type.key = defined_objects[marker.id]["name"]
 			objects_msg.objects.append(object)
 		self.ar_object_publisher.publish(objects_msg)
